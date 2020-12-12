b7751bafe472f20a7995062224d01796a50990e8
alto-I <blmaldiy@gmail.com>
2020-12-12 19:44:19 +0900

    ユーザー一覧ページの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :postal_code, :address, :introduce_yourself])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :postal_code, :address, :introduce_yourself])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[user_name postal_code address introduce_yourself])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[user_name postal_code address introduce_yourself])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
-  def show
+  def index
+    @users = User.all
   end
 
-  private
+  def show; end
 
   def set_user
     @user = User.find(params[:id])
